94ac39da35fa2d784427f3557b8fba2aad4328c8
rocio <rocio.perez.sbarato@mi.unc.edu.ar>
2022-11-01 00:50:43 -0300

    test

---
+++ pyrobots-app/src/containers/Verification/Verification.test.js
@@ -0,0 +1,42 @@
+import { fireEvent, render, screen,  within, act} from "@testing-library/react";
+import { BrowserRouter as Router} from 'react-router-dom';
+import Verification from "./Verification.js";
+
+beforeEach(() => {
+    render(
+        <Router> 
+            <Verification/>
+        </Router> 
+    );
+  });
+
+describe("Componente de verificación de email", () => {
+
+    test("Renderiza el título", () => {
+        const bannerTitle = screen.getByRole('banner');
+        within(bannerTitle).getByText(/pyrobots/i);
+        expect(bannerTitle).toBeInTheDocument();     
+    });
+
+    test("Renderiza el banner", () => {
+        const banner = screen.getByRole('banner');  
+        expect(banner).toBeInTheDocument();     
+    });
+
+    test("Renderiza el alert", () => {
+        const alert = screen.getByRole('alert');  
+        expect(alert).toBeInTheDocument();   
+        const p1 = screen.getByText(/verificación exitosa/i);  
+        expect(p1).toBeInTheDocument();   
+        const p2 = screen.getByText(/tu email ha sido verificado —/i);  
+        expect(p2).toBeInTheDocument();   
+        const p3 = screen.getByText(/ya podés loguearte y jugar pyrobots!/i) ;   
+        expect(p3).toBeInTheDocument();   
+    });
+    
+    test("El botón de login está en el componente", () => {
+        const buttonLogin = screen.getByRole('button', {  name: /login/i});  
+        expect(buttonLogin).toBeInTheDocument();     
+    });
+    
+});
