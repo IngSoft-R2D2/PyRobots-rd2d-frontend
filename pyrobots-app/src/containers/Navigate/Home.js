b850ce4397e6552c768b3c31f0b9624ff9e4183a
unknown <rocio.perez.sbarato@mi.unc.edu.ar>
2022-11-01 19:51:04 -0300

    formato mui

--- pyrobots-app/src/containers/Navigate/Home.js
+++ pyrobots-app/src/containers/Navigate/Home.js
@@ -1,5 +1,13 @@
-import { useNavigate } from "react-router-dom";
-import "./Navigate.css";
+import useNavigate from "react-router-dom";
+import Stack from "@mui/system";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import CreateIcon from '@mui/icons-material/Create';
+import GridOnIcon from '@mui/icons-material/GridOn';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -16,21 +24,79 @@ const Home = () => {
     navigate("/board");
   }
 
-  return (
-      <div>
-        <h1>Binvenide</h1>
-        <h2>Estas son las acciones que puedes elegir</h2>
+  return [
+    <AppBar position="static" sx={{ background: 'dark-blue' }} 
+        style={{
+          marginRight: "950px",
+            padding: "10px 36px"
+        }}>
+      <Toolbar >
+        <SmartToyIcon sx={{ fontSize: "80px" }} />
+        <Typography variant="h3" 
+          component="div" 
+          style={{fontWeight: "700",
+                  fontFamily: "Roboto",
+                  padding: "18px 36px"}}>
+          PyRobots
+        </Typography>
+      </Toolbar>
+    </AppBar>,
+    <Stack>
+      <Typography variant="h4" 
+                  component="div" 
+                  style={{fontWeight: "700",
+                    fontFamily: "Roboto",
+                    padding: "25px 36px"}}>
+      Bienvenide
+      </Typography>
+      <Typography variant="h5" 
+                  component="div" 
+                  style={{fontWeight: "700",
+                          fontFamily: "Roboto",
+                          padding: "10px 36px"}}>
+      Estas son las acciones que puedes elegir
+      </Typography>
         <p>
-          <button onClick={goToMatchForm}>crear partida</button>
+          <Button 
+          style={{
+            color: "#fff",
+            padding: "18px 36px",
+            fontSize: "22px"
+          }}
+          size="medium"
+          variant="contained"  
+          endIcon={<CreateIcon  sx={{ fontSize: "80px" }} /> }
+          sx={{  width: '300px' }}
+          onClick={goToMatchForm}>crear partida</Button>
         </p>
         <p>
-          <button onClick={goToBotForm}>crear Robot</button>
+          <Button 
+          style={{
+            color: "#fff",
+            padding: "18px 36px",
+            fontSize: "22px"
+          }}
+          size="medium"
+          variant="contained"  
+          endIcon={<FileUploadIcon  sx={{ fontSize: "80px" }} /> }
+          sx={{  width: '300px' }}
+          onClick={goToBotForm}>crear Robot</Button>
         </p>
         <p>
-          <button onClick={seeBoard}>ver Tablero</button>
+          <Button 
+          style={{
+              color: "#fff",
+              padding: "18px 36px",
+              fontSize: "22px"
+            }}
+            size="medium"
+            variant="contained"  
+            endIcon={<GridOnIcon  sx={{ fontSize: "80px" }} /> }
+            sx={{  width: '300px' }}
+            onClick={seeBoard}>ver Tablero</Button>
         </p>
-      </div>
-    );
+      </Stack>
+  ];
 };
 
 export default Home;
--- pyrobots-app/src/containers/Navigate/Root.js
+++ pyrobots-app/src/containers/Navigate/Root.js
@@ -1,7 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import "./Navigate.css";
+import { Stack } from "@mui/system";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import PersonIcon from '@mui/icons-material/Person';
+import LoginIcon from '@mui/icons-material/Login';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+import Typography from '@mui/material/Typography';
 
 const Root = () => {
+
   const navigate = useNavigate();
 
   const goToSignUp = async() => {
@@ -12,17 +20,60 @@ const Root = () => {
     navigate("/login");
   }
 
-  return (
-      <div>
-        <h1>Binvenide a PyRobots</h1>
+  return [
+    <AppBar position="static" sx={{ background: 'dark-blue' }} 
+        style={{
+          marginRight: "950px",
+            padding: "10px 36px"
+        }}
+    >
+      <Toolbar >
+        <SmartToyIcon sx={{ fontSize: "80px" }} />
+        <Typography variant="h3" 
+                      component="div" 
+                      style={{fontWeight: "700",
+                        fontFamily: "Roboto",
+                        padding: "18px 36px"}}>
+        PyRobots
+        </Typography>
+        </Toolbar>
+      </AppBar>,
+      <Stack>
+        <Typography variant="h4" 
+                      component="div" 
+                      style={{fontWeight: "700",
+                        fontFamily: "Roboto",
+                        padding: "30px 36px"}}>
+        Bienvenide
+        </Typography>
         <p>
-          <button onClick={goToSignUp}>Registrarme</button>
-        </p>
-        <p>
-          <button onClick={goToLogin}>Loguearme</button>
-        </p>
-      </div>
-    );
+          <Button 
+            style={{
+              color: "#fff",
+              padding: "18px 36px",
+              fontSize: "22px"
+            }}
+            size="medium"
+            variant="contained" 
+            endIcon={<PersonIcon  sx={{ fontSize: "large" }} /> }
+            sx={{  width: '300px' }}
+            onClick={goToSignUp}>Registrarme</Button>
+            </p>
+          <p>
+          <Button 
+            style={{
+              color: "#fff",
+              padding: "18px 36px",
+              fontSize: "22px"
+            }}
+            size="medium"
+            variant="contained"  
+            endIcon={<LoginIcon  sx={{ fontSize: "large" }} /> }
+            sx={{  width: '300px' }}
+            onClick={goToLogin}>Loguearme</Button>
+          </p>
+      </Stack>
+    ];
 };
 
 export default Root;
