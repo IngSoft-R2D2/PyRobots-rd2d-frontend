098a67ef20f710f35d37496ba6468b637abf12e8
Benjamin Alvarez <benjamin.alvarez@mi.unc.edu.ar>
2022-11-09 21:51:57 -0300

    idea

--- pyrobots-app/src/containers/Board/Board.js
+++ pyrobots-app/src/containers/Board/Board.js
@@ -1,52 +1,30 @@
 import React from 'react';
-import { Stage, Layer, Circle, Rect } from 'react-konva';
+import { Stage, Layer, Circle, Rect, Text } from 'react-konva';
 import './Board.css';
-import { Stack } from "@mui/system";
-import { Button }  from "@mui/material";
-import { useNavigate } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import SmartToyIcon from '@mui/icons-material/SmartToy';
-import UndoIcon from '@mui/icons-material/Undo';
 
-  const Board = () => { 
-    const navigate = useNavigate();
-    const goBack= async() => {
-        navigate("/home");
-    }
-    return (
-      <Stack>
-        <AppBar position="static" sx={{ background: 'dark-blue' }} 
-        style={{
-            marginRight: 500,
-            padding: "18px 36px"
-        }}
-        key = {0} >
-        <Toolbar sx={{ justifyContent: "space-between" }}>
-          <SmartToyIcon sx={{ fontSize: "80px" }} />
-          <Typography variant="h3" 
-                      component="div" 
-                      style={{fontWeight: "500",
-                        fontFamily: "Roboto",
-                        padding: "18px 36px"}}>
-            Simulación
-          </Typography>
-          <Button 
-              style={{
-                color: "#fff",
-                padding: "18px 36px",
-                fontSize: "22px"
-              }}
-              variant="secondary" 
-              endIcon={<UndoIcon  sx={{ fontSize: "large" }} /> } 
-              onClick={goBack} 
-              sx={{ marginLeft: "auto" }}>
-              Volver atrás
-          </Button>
-        </Toolbar>
-      </AppBar>
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+const Board = (props) => { 
+  const ronda = props.round; 
+  const finished = props.finished; 
+
+  return (
+    finished ? 
+    // caso se acaba la animacion 
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+        <Text x={600} y={50} fontSize={50} text = "simulacion terminada"/>
+          <Rect
+            x={20}
+            y={50}
+            width={500}
+            height={500}
+            fill= "rgb(68, 189, 92)"
+            shadowBlur={10}
+          />
+      </Layer>
+    </Stage> : 
+    // caso animacion normal 
+    
+    <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Rect
             x={20}
@@ -56,16 +34,18 @@ import UndoIcon from '@mui/icons-material/Undo';
             fill= "rgb(68, 189, 92)"
             shadowBlur={10}
           />
-         
-          <Circle x={300} y={460} radius={10} fill="red" stroke="black"/>
-          <Circle x={100} y={300} radius={10} fill="hotpink" stroke="black"/>
-          <Circle x={130} y={340} radius={10} fill="yellow" stroke="black"/>
-          <Circle x={450} y={490} radius={10} fill="blue" stroke="black"/>
+       {Object.keys(ronda).map((robot)=>(
+       <Circle x={ronda[robot].position[0]/2+20} y={ronda[robot].position[1]/2+50} radius={10} fill={'#'+parseInt(robot)} stroke="black" key={robot} text="123"/>
+        ))}
+       {Object.keys(ronda).map((robot)=>(
+       <Text x={600} y={50} fontSize={20} text = {"nombre: "+robot+" damage: "+ronda[robot].damage}/>
+       ))}
+        <Text x={600} y={70} fontSize={20} text = {"nombre: "+"R1"+" damage: "+ronda["R1"].damage}/>
 
-        </Layer>
-      </Stage>
-      </Stack>
-    );
+      </Layer>
+    </Stage>
+    // </Stack>
+  );
 }
 
 export default Board;
