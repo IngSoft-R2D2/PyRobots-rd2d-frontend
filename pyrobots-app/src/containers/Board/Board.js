6fe5437451f24524cd198d78f51cca13684c4ab9
benjaminalv00 <benjamin.alvarez@unc.edu.ar>
2022-11-12 14:23:48 -0300

    cambio cuando robot muere

--- pyrobots-app/src/containers/Board/Board.js
+++ pyrobots-app/src/containers/Board/Board.js
@@ -8,26 +8,34 @@ const Board = (props) => {
   const colors = ["red","blue","brown","green"]
 
   return (
-    
     <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Rect
-            x={20}
+            x={30}
             y={50}
-            width={510}
-            height={510}
+            width={530}
+            height={520}
             fill= "rgb(68, 189, 92)"
             shadowBlur={10}
           />
-       {Object.keys(ronda).map((robot)=>(
-       <Circle 
-          x={ronda[robot].position[0]/2+20} 
-          y={ronda[robot].position[1]/2+50} 
-          radius={10} 
-          fill={colors[array_ronda.indexOf(robot)]} 
-          stroke="black" 
-          key={robot} />
-        ))}
+       {Object.keys(ronda).map((robot)=>( (ronda[robot].damage===100) ? 
+        <Text 
+            fontFamily={'Roboto'}
+            fill = {colors[array_ronda.indexOf(robot)]}
+            x={ronda[robot].position[0]/2+30} 
+            y={ronda[robot].position[1]/2+50} 
+            fontSize={20} 
+            text = {"X"}
+            key={robot}/>
+          :
+        <Circle 
+            x={ronda[robot].position[0]/2+40} 
+            y={ronda[robot].position[1]/2+50} 
+            radius={10} 
+            fill={colors[array_ronda.indexOf(robot)]} 
+            stroke="black" 
+            key={robot} />
+          ))}
        {Object.keys(ronda).map((robot)=>(
        <Text 
           fontFamily={'Roboto'}
@@ -42,7 +50,7 @@ const Board = (props) => {
        {Object.keys(ronda).map((robot)=>(
        <Rect 
           key={robot}
-          x={1000} 
+          x={1200} 
           y={50*array_ronda.indexOf(robot)+50}   
           width={500-5*+ronda[robot].damage}
           height={50}
