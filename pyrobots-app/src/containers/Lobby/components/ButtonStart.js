969dfad3b0c87da3c54b513d24149028a5d4adbf
rocio-perez-sbarato <rocio.perez.sbarato@mi.unc.edu.ar>
2022-11-12 16:58:25 -0300

    Theme, Auth y test

--- pyrobots-app/src/containers/Lobby/components/ButtonStart.js
+++ pyrobots-app/src/containers/Lobby/components/ButtonStart.js
@@ -2,27 +2,57 @@ import * as React from 'react';
 import PlayCircleFilledWhiteIcon from 
 '@mui/icons-material/PlayCircleFilledWhite';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import { fetchToken } from '../elements/Auth.js';
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      start: {
+        light: '#6fbf73',
+        main: '#4caf50',
+        dark: '#357a38',
+        contrastText: '#000',
+      },
+    },
+  });
 
 const Start = (props) => {
-    /*const match_id = props.match_id
+    const navigate = useNavigate();
+    const match_id = props.match_id
     const startMatch = async() => {
+        const token = fetchToken();
         try{
-                const response = await fetch("http://localhost:8000/matches/start/${match_id}", {
+                const response = await fetch("http://localhost:8000/matches/join/?id=${match_id}", {
                 method: "PUT",
-                headers: { "Content-Type": "application/json" },
+                headers: { "accept": "application/json",
+                            'Authorization': `Bearer ${token}`},
                 body: JSON.stringify({})
                 })
-        const data = await response.json();
+            const data = await response.json();
+            if(response.ok){
+                alert("Partida iniciada exitosamente")
+                // setTimeout(() => {
+                // navigate('/lobby')
+                // }, 4000);
+            }
+            else {
+                alert(data.detail)
+            }
         }catch(error){
             alert(error)
         }
-    }*/
+    }
     return(
-        <Button variant="contained" size="medium"
-            endIcon={<PlayCircleFilledWhiteIcon sx={{ fontSize: "800px" }} /> }
-            onClick={startMatch} > 
-            Iniciar
-        </Button>
+        <ThemeProvider theme={theme}>
+            <Button 
+                color="start" 
+                variant="outlined" size="medium"
+                endIcon={<PlayCircleFilledWhiteIcon sx={{ fontSize: "large" }} /> }
+                onClick={startMatch} > 
+                Iniciar
+            </Button>
+        </ThemeProvider>
     )
 }
 
