b2ce8dae78e893582871812e513eb2a9ba9d7b5a
rocio-perez-sbarato <rocio.perez.sbarato@mi.unc.edu.ar>
2022-11-12 16:33:41 -0300

    Theme, Auth y test

--- pyrobots-app/src/containers/Lobby/components/ButtonLeave.js
+++ pyrobots-app/src/containers/Lobby/components/ButtonLeave.js
@@ -1,27 +1,58 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import DangerousIcon from '@mui/icons-material/Dangerous';
+import { useNavigate } from 'react-router-dom';
+import { fetchToken } from '../elements/Auth.js';
+import Button from '@mui/material/Button';
+
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      leave: {
+        light: '#ea605d',
+        main: '#e53935',
+        dark: '#a02725',
+        contrastText: '#000',
+      },
+    },
+  });
 
 const Leave = (props) => {
-    /*const match_id = props.match_id
+    const navigate = useNavigate();
+    const match_id = props.match_id
     const joinMatch = async() => {
+        const token = fetchToken();
         try{
-                const response = await fetch("http://localhost:8000/matches/join/${match_id}", {
+                const response = await fetch("http://localhost:8000/matches/join/?id=${match_id}", {
                 method: "PUT",
-                headers: { "Content-Type": "application/json" },
+                headers: { "accept": "application/json",
+                            'Authorization': `Bearer ${token}`},
                 body: JSON.stringify({})
                 })
-        const data = await response.json();
+            const data = await response.json();
+            if(response.ok){
+                alert("Partida abandonada exitosamente")
+                setTimeout(() => {
+                navigate('/')
+                }, 4000);
+            }
+            else {
+                alert(data.detail)
+            }
         }catch(error){
             alert(error)
         }
-    }*/
+    }
     return(
-        <Button variant="contained" size="medium"
-            endIcon={<DangerousIcon sx={{ fontSize: "800px" }} /> }
-            onClick={joinMatch} > 
-            Iniciar
-        </Button>
+        <ThemeProvider theme={theme}>
+            <Button 
+                color= "leave"
+                variant="outlined" size="large"
+                endIcon={<DangerousIcon sx={{ fontSize: "large" }} /> }
+                onClick={joinMatch} > 
+                Abandonar partida
+            </Button>
+        </ThemeProvider>
     )
 }
 
