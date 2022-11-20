d00cc10285e543ccaefcf88b4f611569dbffa97e
rocio-perez-sbarato <rocio.perez.sbarato@mi.unc.edu.ar>
2022-11-19 21:28:33 -0300

    Arreglo de appbar y  cambio de diseño en tabla

--- pyrobots-app/src/containers/Lobby/components/Results.js
+++ pyrobots-app/src/containers/Lobby/components/Results.js
@@ -1,7 +1,4 @@
 import Typography from '@mui/material/Typography';
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -20,45 +17,66 @@ import CreateIcon from '@mui/icons-material/Create';
 import HomeIcon from '@mui/icons-material/Home';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
- import TableCell, { tableCellClasses } from '@mui/material/TableCell';
- import TableContainer from '@mui/material/TableContainer';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
- import TableRow from '@mui/material/TableRow';
- import Paper from '@mui/material/Paper';
- import { styled } from '@mui/material/styles';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@mui/material/Grid';
 
- const StyledTableCell = styled(TableCell)(({ theme }) => ({
-   [`&.${tableCellClasses.head}`]: {
-     backgroundColor: theme.palette.common.black,
-     color: theme.palette.common.white,
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 650,
   },
-   [`&.${tableCellClasses.body}`]: {
-     fontSize: 20,
-   },
- }));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-   '&:nth-of-type(odd)': {
-     backgroundColor: theme.palette.action.hover,
+  tableContainer: {
+    borderRadius: 15,
+    margin: '10px 10px',
+    maxWidth: 950
   },
-   // hide last border
-  '&:last-child td, &:last-child th': {
-     border: 0,
-   },
- }));
-
-const useStyles = makeStyles(theme => ({
-  title: {
-    flexGrow: 1,
-    textAlign: 'left',
+  tableHeaderCell: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      backgroundColor: theme.palette.primary.dark,
+      color: theme.palette.getContrastText(theme.palette.primary.dark)
   },
-  typography: {
-    allVariants: {
-      color: "pink"
-    },
+  avatar: {
+      backgroundColor: theme.palette.primary.dark,
+      color: theme.palette.getContrastText(theme.palette.primary.dark)
   },
+  name: {
+    fontWeight: 'bold',
+    color: theme.palette.info.dark
+},
 }));
+
+//  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+//    [`&.${tableCellClasses.head}`]: {
+//      backgroundColor: theme.palette.common.black,
+//      color: theme.palette.common.white,
+//   },
+//    [`&.${tableCellClasses.body}`]: {
+//      fontSize: 20,
+//    },
+//  }));
+
+// const StyledTableRow = styled(TableRow)(({ theme }) => ({
+//    '&:nth-of-type(odd)': {
+//      backgroundColor: theme.palette.action.hover,
+//   },
+//    // hide last border
+//   '&:last-child td, &:last-child th': {
+//      border: 0,
+//    },
+//  }));
+
+// const useStyles = makeStyles(theme => ({
+//   title: {
+//     flexGrow: 1,
+//     textAlign: 'left',
+//   },
+// }));
 
 const Results = (props) => {
   const navigate = useNavigate();
@@ -81,15 +99,17 @@ const Results = (props) => {
     <AppBar position="fixed" sx={{ background: 'dark-blue' }} 
       key = {0} >
       <Toolbar style={{display:'flex', justifyContent:"space-between", width:'100%'}}>    
-        <SmartToyIcon sx={{ fontSize: "80px" }} />
-        <Typography variant="h3" 
-                    component="div" 
-                    style={{fontWeight: "700",
+      <Box display='flex' flexGrow={1}>
+          <SmartToyIcon sx={{ fontSize: "80px" }} />
+          <Typography variant="h3" 
+                      component="div" 
+                      style={{fontWeight: "700",
                       fontFamily: "Roboto",
                       padding: "18px 36px"}} 
-                      className={classes.title}>
-          PyRobots
-        </Typography>
+          >
+            PyRobots
+          </Typography>
+        </Box>  
         <ButtonGroup variant="secondary" aria-label="outlined primary button group">
         <Button 
           style={{
@@ -131,86 +151,40 @@ const Results = (props) => {
       display="flex"
       justifyContent="center"
       alignItems="left"
-      minHeight="18vh"
+      minHeight="35vh"
       key = {1}
       >
       <Stack direction="row" alignItems="center" gap={1} >
         <CheckCircleIcon style={{ color: "#1C8E40"}}/>
         <Typography variant="h5" style={{fontWeight: "700",
-        fontFamily: "Roboto",
-        padding: "18px 0px"}}>Partida finalizada!</Typography>
+        fontFamily: "Roboto"}}>Partida finalizada!</Typography>
       </Stack>
       </Box>,
-      <Box
-      display="flex"
-      justifyContent="flex-start"
-      alignItems="left"
-      minHeight="25vh"
-      key = {2}
-      >
-        {/* <Stack> */}
-        <Typography variant="h6" gutterBottom
-        style={{fontWeight: "500",
-        fontFamily: "Roboto",
-        padding: "20px 36px"}}>
-          {`Ganador/es `}
-        </Typography>
-        {/* <List sx={{ width: "100%", maxWidth: 360, bgcolor: "white" }}>
-          {props.results.map((value) => (
-            <Stack> 
-              <ListItem>
-              <ListItemAvatar>
-              <Avatar style={{ backgroundColor: "#ff6f00" }}
-              sx={{ height: '20px', width: '20px' }}>
-                <StarsIcon style={{ fontSize: 20 }}/>
-              </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={`${value.user_name}`} secondary={`${value.robot_name}`} 
-              primaryTypographyProps={{ style: { fontSize: '1rem', fontFamily: "Roboto" } }}/>
-              </ListItem>
-              <Divider variant="inset" component="li" />
-            </Stack>
-          ))}
-        </List>
-        </Stack> */}
-    <TableContainer component={Paper}>
-     <Table sx={{ minWidth: 700 }} aria-label="customized table">
+      <TableContainer component={Paper} className={classes.tableContainer}>
+      <Table className={classes.table} aria-label="simple table">
          <TableHead>
            <TableRow>
-            
-            <StyledTableCell>Jugadores</StyledTableCell>
-             <StyledTableCell align="right">Juegos perdidos</StyledTableCell>
-             <StyledTableCell align="right">Juegos ganados</StyledTableCell>
+           <TableCell className={classes.tableHeaderCell}>Jugadores</TableCell>
+            <TableCell  align="right" className={classes.tableHeaderCell}>Juegos ganados</TableCell>
+            <TableCell  align="right" className={classes.tableHeaderCell}>Juegos perdidos</TableCell>
           </TableRow>
          </TableHead>
          <TableBody>
           {props.results.map((row) => (
-             <StyledTableRow key={row.user_name}>
-              <StyledTableCell scope="row">
-              <Stack direction="column">
-              <Typography variant="h6" gutterBottom
-                style={{fontWeight: "700",
-                fontFamily: "Roboto",
-                padding: "20px 36px"}}>
-                  {row.user_name}
-              </Typography>
-              <Typography variant="h7" gutterBottom
-                style={{fontWeight: "500",
-                fontFamily: "Roboto",
-                padding: "20px 36px"}}>
-                  {row.robot_name}
-              </Typography>
-              </Stack>
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.won_games}</StyledTableCell>
-              <StyledTableCell align="right">{row.lost_games}</StyledTableCell>
-             </StyledTableRow>
+             <TableRow key={row.user_name}>
+              <TableCell scope="row">
+              <Grid item lg={10}>
+                <Typography className={classes.name}>{row.user_name}</Typography>
+                <Typography color="textSecondary" variant="body2">{row.robot_name}</Typography>
+              </Grid>
+              </TableCell>
+              <TableCell align="right">{row.won_games}</TableCell>
+              <TableCell align="right">{row.lost_games}</TableCell>
+             </TableRow>
           ))}
          </TableBody>
        </Table>
     </TableContainer>
-
-      </Box>
   ];
 }
 
