4ee83eb230e5cfc27201d67bb5ca23e202b2bf15
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-11-21 00:30:00 -0300

    select algo mejorado

--- pyrobots-app/src/containers/Bot/BotSelect.js
+++ pyrobots-app/src/containers/Bot/BotSelect.js
@@ -5,64 +5,135 @@ import BotList from "./components/BotList.js";
 import ButtonJoin from "../MatchesList/components/ButtonJoin.js"
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import AppBar from '@mui/material/AppBar';
+import { Stack } from "@mui/system";
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+import UndoIcon from '@mui/icons-material/Undo';
+import { Button } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
-const BotSelect = ()=> {
-    
-    const [loading, setLoading] = useState(true)
-    const [robot, setRobot] = useState({robot_id:''})
-    const [robots, setRobots] = useState([]);
-    const location = useLocation();
-    const match_id = location.state.m_id;
-    const players = location.state.players;
-    const user_id = location.state.user_id;
-    const is_creator = location.state.user_is_creator;
-    const user_name = location.state.user_name;
-    const is_started = location.state.is_started;
 
-    useEffect(() => { 
-        const token = fetchToken();
-        fetch(`http://localhost:8000/robots`,{
-            method: "GET",
-            headers: {'accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}` },
-        })
-         .then((response) => response.json())
-         .then ((data)=> {
-            setRobots(data);
-            setLoading(false);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-       }, []);
+const BotSelect = () => {
 
-       
+  const [loading, setLoading] = useState(true)
+  const [robot, setRobot] = useState({ robot_id: '' })
+  const [robots, setRobots] = useState([]);
+  const location = useLocation();
+  const match_id = location.state.m_id;
+  const players = location.state.players;
+  const user_id = location.state.user_id;
+  const is_creator = location.state.user_is_creator;
+  const user_name = location.state.user_name;
+  const is_started = location.state.is_started;
+  const navigate = useNavigate();
+  const goBack = async () => {
+    navigate("/home");
+  }
 
-    return (
-        loading ?  
-        <Backdrop
-            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-            open={loading}
-            >
-            <CircularProgress color="inherit" />
-        </Backdrop> :
-            <div>
-                <BotList 
-                robot = {robot}
-                setRobot = {setRobot} 
-                robots = {robots}
-                />
-                <ButtonJoin match_id={match_id} 
-                            robot_id={robot.robot_id} 
-                            players={players} 
-                            user_id={user_id} 
-                            is_creator={is_creator} 
-                            user_name = {user_name}
-                            is_started = {is_started}/>
-            </div>
-    )
+  useEffect(() => {
+    const token = fetchToken();
+    fetch(`http://localhost:8000/robots`, {
+      method: "GET",
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setRobots(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+
+  return (
+    loading ?
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop> :
+      <div style={{
+        display: "flex",
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        top: 0,
+        left: 0
+      }}>
+        <AppBar position="fixed" sx={{ background: 'dark-blue' }}
+          style={{
+            marginRight: '0px',
+            padding: "18px 36px"
+          }}
+          key={0} >
+          <Toolbar sx={{ justifyContent: "space-between" }}>
+            <SmartToyIcon sx={{ fontSize: "80px" }} />
+            <Typography variant="h3"
+              component="div"
+              style={{
+                fontWeight: "500",
+                fontFamily: "Roboto",
+                padding: "18px 36px"
+              }}>
+              Selección de Robots
+            </Typography>
+            <Button
+              style={{
+                color: "#fff",
+                padding: "18px 36px",
+                fontSize: "22px"
+              }}
+              variant="secondary"
+              endIcon={<UndoIcon sx={{ fontSize: "large" }} />}
+              onClick={goBack}
+              sx={{ marginLeft: "auto" }}>
+              Volver atrás
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <div>
+          <Stack
+            spacing={20}
+            direction='row'
+            style={{
+              height: "100%",
+              width: "100%",
+              padding: "10px 36px"
+            }}
+          >
+            <BotList
+              robot={robot}
+              setRobot={setRobot}
+              robots={robots}
+            />
+            
+              <ButtonJoin match_id={match_id}
+                robot_id={robot.robot_id}
+                players={players}
+                user_id={user_id}
+                is_creator={is_creator}
+                user_name={user_name}
+                is_started={is_started}
+              />
+            
+          </Stack>
+        </div>
+      </div>
+  )
 }
 
 export default BotSelect;
