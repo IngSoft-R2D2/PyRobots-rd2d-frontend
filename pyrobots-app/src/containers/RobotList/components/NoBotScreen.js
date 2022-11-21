3594333b8cb8992d392ed1ef87b2a3271563b7d9
Mati Teruel <matias.teruel@mi.unc.edu.ar>
2022-11-21 13:17:07 -0300

    centrar no bot screen

--- pyrobots-app/src/containers/RobotList/components/NoBotScreen.js
+++ pyrobots-app/src/containers/RobotList/components/NoBotScreen.js
@@ -1,4 +1,4 @@
-import { Alert,Button} from '@mui/material';
+import { Alert, Button } from '@mui/material';
 import { AlertTitle } from '@mui/material';
 import SmartToyIcon from '@mui/icons-material/SmartToy';
 import UndoIcon from '@mui/icons-material/Undo';
@@ -7,30 +7,48 @@ import { useNavigate } from 'react-router-dom';
 
 
 const NoBotScreen = () => {
-    const navigate = useNavigate();
-    const goBack= async() => {
-        navigate("/home");
-    }
-    const goToRobot= async() => {
-        navigate("/robots");
-    }
-    return(
-        <Stack
-            spacing={2}
-            >
-            <Alert severity="error"
-                style={{
-                padding: "18px 36px",
-                }}>
-                <AlertTitle>No se pueden listar tus robots</AlertTitle>
-                Para listarlos debes tener al menos un robot
-            </Alert> 
-            <Button variant="contained" size="large" endIcon={<UndoIcon fontSize="large"/> } 
-                onClick={goBack} > Volver atras 
-            </Button>
-            <Button variant="contained" size="large" endIcon={<SmartToyIcon fontSize="large"/> } 
-            onClick={goToRobot} > Crear robot
-            </Button>
-        </Stack> 
-)};
+  const navigate = useNavigate();
+  const goBack = async () => {
+    navigate("/home");
+  }
+  const goToRobot = async () => {
+    navigate("/robots");
+  }
+  return (
+    <Stack
+      spacing={2}
+      direction='column'
+      style={{
+        display: "flex-start",
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        background: "white",
+        top: 0,
+        left: 0
+      }}
+    >
+      <Alert severity="error"
+        style={{
+          padding: "18px 36px",
+        }}>
+        <AlertTitle>No se pueden listar tus robots</AlertTitle>
+        Para listarlos debes tener al menos un robot
+      </Alert>
+      <Stack
+        direction='row'
+        spacing={4}
+      >
+        <Button variant="contained" size="large" endIcon={<UndoIcon fontSize="large" />}
+          onClick={goBack} > Volver atras
+        </Button>
+        <Button variant="contained" size="large" endIcon={<SmartToyIcon fontSize="large" />}
+          onClick={goToRobot} > Crear robot
+        </Button>
+      </Stack>
+    </Stack>
+  )
+};
 export default NoBotScreen;
